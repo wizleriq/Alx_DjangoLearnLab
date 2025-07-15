0f5a927124199aa9a11d15e7633c43122a7ee397
Ndata <wisdomndata@gmail.com>
2025-07-15 16:50:34 +0100

    Django

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1qk^$)678c-2!)oqd%4p!&7g)z*e2h_%agzn14#fbr%-#9v9p2'
+SECRET_KEY = 'django-insecure-=n$w^$6h&9hfky=yx8p#bwcb)itjf5u0jppkof1ks5)ohj%m$i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- Introduction_to_Django/LibraryProject/README.md
+++ Introduction_to_Django/LibraryProject/README.md
@@ -1,11 +1,2 @@
-# LibraryProject 📚
-
-This is a simple Django project created to learn how Django works.
-
-## How to Run
-
-1. Make sure you have Python and Django installed.
-2. Run the development server:
-
-```bash
-python manage.py runserver
+LibraryProject
+This is a basic Django project created to practice the Django setup process.
