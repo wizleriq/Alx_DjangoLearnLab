13d8c43f85453bd48df58f2b2dd76f9b938d39f4
Ndata <wisdomndata@gmail.com>
2025-07-28 19:52:16 +0100

    New Task

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,16 @@
+# from django.urls import path
+# from .views import book_list_view, LibraryDetailView
+
+# urlpatterns = [
+#     path('books/', book_list_view, name='book-list'),
+#     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
+# ]
+
+
 from django.urls import path
-from .views import book_list_view, LibraryDetailView
+from .views import list_books, LibraryDetailView  
 
 urlpatterns = [
-    path('books/', book_list_view, name='book-list'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
+    path('books/', list_books, name='list_books'),  
+    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'), 
 ]
