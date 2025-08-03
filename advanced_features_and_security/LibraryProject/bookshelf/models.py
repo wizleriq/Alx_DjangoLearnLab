320a226a3e3744464a7c4aa92246a58a6ccc1c3f
Ndata <wisdomndata@gmail.com>
2025-08-03 19:37:20 +0100

    Django

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -60,7 +60,7 @@ class CustomUserManager(BaseUserManager):
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-    objects = CustomUserManager
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.username
