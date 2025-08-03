8f69c2deb5ad4c6dd679b564d4075925fed9f352
Ndata <wisdomndata@gmail.com>
2025-08-03 15:15:20 +0100

    Django Task

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import permission_required
+from.models import Book
 
 # Create your views here.
-from django.http import HttpResponse
-
 def home(request):
     return HttpResponse("Welcome to the home page")
+
+@permission_required('bookshelf.view_book, raise_exception=True')
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
