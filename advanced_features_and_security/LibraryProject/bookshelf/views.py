d4874c889bfbb64d0ef42fc6b84c36c42cdb3d2d
Ndata <wisdomndata@gmail.com>
2025-08-03 19:26:17 +0100

    Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import permission_required
 from.models import Book
+from .forms import ExampleForm
 
 # Create your views here.
+def example_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_void():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            return HttpResponse(f"Thanks, {name}!")
+    else:
+        form = ExampleForm()
+
+    return render(request, 'example_template.html', {'form': form})
+
 def home(request):
     return HttpResponse("Welcome to the home page")
 
