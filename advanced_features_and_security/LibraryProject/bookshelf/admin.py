832a1777b538e927d17df37e9ff261b23418e00b
Ndata <wisdomndata@gmail.com>
2025-08-03 13:15:46 +0100

    New code

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,16 +1,26 @@
 from django.contrib import admin
 from .models import Book
 from .models import CustomUser
-# from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     list_filter = ('publication_year', 'author')  # This is a tuple
     search_fields = ('title', 'author')
 
-# class CustomUserAdmin(admin.UserAdmin):
-#     pass
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
 
 admin.site.register(Book, BookAdmin)
 
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
