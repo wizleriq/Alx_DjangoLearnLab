4581a05e6f9b1619220d9913d8b1893d2f53f551
Ndata <wisdomndata@gmail.com>
2025-08-09 19:10:59 +0100

    Api Task

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -10,7 +10,7 @@ from .views import(
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('books/create/', BookCreateView.as_view, name='book-create'),
-    path('book/<int:pk>/update/', BookUpdateView.as_view, name='book-update'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('book/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
     path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete')
 ]
