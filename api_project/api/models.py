a9af7b2b951cf51ba8c2e13f7e8bf36a8b92f82c
Ndata <wisdomndata@gmail.com>
2025-08-16 22:09:50 +0100

    new

---
+++ api_project/api/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class BookModel(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
