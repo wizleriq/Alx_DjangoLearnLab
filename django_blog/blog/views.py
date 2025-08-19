bfb2dec837eba8b6f45838cedfc238481d6322d8
Ndata <wisdomndata@gmail.com>
2025-08-19 01:05:51 +0100

    Django CRUD

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,6 +3,68 @@ from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
 from .forms import CustomUserCreationForm
+from rest_framework import views, generics
+from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from .models import Post
+
+#CRUD operations
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/list.html"
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/detail.html"
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = "blog/create.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = "blog/update.html"
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user== post.user
+    
+class PostDeletwView(LoginRequiredMixin,  UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "blog/delete.html"
+    success_url = reverse_lazy("post-list")
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+# class PostListView(generics.ListAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+# class PostDetailView(generics.RetrieveAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+# class PostCreateView(generics.CreateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#     authentication_classes = [TokenAuthentication]
+#     permission_classes = IsAuthenticated
+
+# class PostUpdateView(generics.UpdateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+# class PostDeleteView(generics.DestroyAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
 
 # Registration view
 def register(request):
