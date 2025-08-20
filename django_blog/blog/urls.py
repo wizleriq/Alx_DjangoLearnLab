419c0ae88a22f52f0907226b9843dbca7ac6b9c2
Ndata <wisdomndata@gmail.com>
2025-08-20 11:13:05 +0100

    Django Crud

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path("post/new/", PostCreateView.as_view(), name='post-create'),
     path("/post/int:pk>/", PostDetailView.as_view(), name='post-detail'),
     path("/post/<int:pk>/update/", PostUpdateView.as_view(), name='post-update'),
-    path("/post/<int:pk>/delete", PostDeleteView.as_view(), name='post-delete'),
+    path("/post/<int:pk>/delete/", PostDeleteView.as_view(), name='post-delete'),
 
     # Auth & profile URLs
     path("register/", register, name="register"),
