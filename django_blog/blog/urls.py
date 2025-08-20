4d21606158f0fef47209783849fa86f5ba7c7241
Ndata <wisdomndata@gmail.com>
2025-08-20 11:12:32 +0100

    Django Crud

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,9 +17,9 @@ urlpatterns = [
     # Post CRUD URLs
     path("posts/", PostListView.as_view(), name='post-list'),
     path("post/new/", PostCreateView.as_view(), name='post-create'),
-    path("/posts/int:pk>/", PostDetailView.as_view(), name='post-detail'),
-    path("/posts/<int:pk>/edit/", PostUpdateView.as_view(), name='post-update'),
-    path("/posts/<int:pk>/delete", PostDeleteView.as_view(), name='post-delete'),
+    path("/post/int:pk>/", PostDetailView.as_view(), name='post-detail'),
+    path("/post/<int:pk>/update/", PostUpdateView.as_view(), name='post-update'),
+    path("/post/<int:pk>/delete", PostDeleteView.as_view(), name='post-delete'),
 
     # Auth & profile URLs
     path("register/", register, name="register"),
