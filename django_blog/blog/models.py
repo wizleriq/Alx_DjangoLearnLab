6e4cabaae5559412a1e0b4feeb13cd8f5a8ecaa9
Ndata <wisdomndata@gmail.com>
2025-08-21 11:39:28 +0100

    New

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -10,9 +10,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comment')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "f cmmment by {self.author.username} on {self.post.title}"
