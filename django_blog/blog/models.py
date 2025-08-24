7de5612801f91ef8a5d41449d89a180831c647c8
Ndata <wisdomndata@gmail.com>
2025-08-24 11:39:39 +0100

    Djanjo

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,14 +2,33 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+    
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
-
+    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
     def __str__(self):
         return self.title
+# ✅ Option 2: Use a string reference
+
+# If you want to keep Tag defined below, you can reference it as a string in ManyToManyField:
+
+# class Post(models.Model):
+#     title = models.CharField(max_length=200)
+#     content = models.TextField()
+#     published_date = models.DateTimeField(auto_now_add=True)
+#     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
+#     tags = models.ManyToManyField("Tag", related_name='posts', blank=True)  # 👈 string ref
+
+#     def __str__(self):
+#         return self.title
     
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comment')
@@ -19,4 +38,8 @@ class Comment(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return "f cmmment by {self.author.username} on {self.post.title}"
+        return f"Comment by {self.author.username} on {self.post.title}"
+# class Tag(models.Model):
+#     Tag = models.ForeignKey(Post, on_delete=models.CASCADE)
+#     title = models.CharField(max_length=100)
+    
