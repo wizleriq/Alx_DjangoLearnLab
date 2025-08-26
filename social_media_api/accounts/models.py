25d137906f1126535f61a880a92b24824eed6477
Ndata <wisdomndata@gmail.com>
2025-08-27 00:09:10 +0100

    backend

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,16 +1,30 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-# Create your models here.
 class CustomUser(AbstractUser):
-    bio = models.CharField(max_length=100)
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True,  null=True)
-    followers = models.ManyToManyField(
-        'self',
-        symmetrical=False,
-        related_name = 'following',
-        blank=True
-    )
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
+    followers = models.ManyToManyField("self", symmetrical=False, related_name="following", blank=True)
 
     def __str__(self):
         return self.username
+
+
+
+
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
+
+# # Create your models here.
+# class CustomUser(AbstractUser):
+#     bio = models.CharField(max_length=100)
+#     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True,  null=True)
+#     followers = models.ManyToManyField(
+#         'self',
+#         symmetrical=False,
+#         related_name = 'following',
+#         blank=True
+#     )
+
+#     def __str__(self):
+#         return self.username
