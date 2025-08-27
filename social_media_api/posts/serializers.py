b5b10c94f6deb81dd99c36d80a5c7c47bd2ba8bf
Ndata <wisdomndata@gmail.com>
2025-08-27 18:08:23 +0100

    Social Media API

---
+++ social_media_api/posts/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author-username')
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'title', 'content', 'created_at', 'updated_at']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+    post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all())
+    class Meta: 
+        model = Post
+        fields = ['id','post', 'author', 'content', 'created_at', 'updated_at']
+
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DetailView
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
 # Create your views here.
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True 
+        return obj.author == request.user
+    
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.object.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perfrom_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.object.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
