9ba6f3a6ee5402d2b0d70d1c8bb97449aa3c27e3
Ndata <wisdomndata@gmail.com>
2025-09-03 01:33:13 +0100

    Social Media API

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     author = models.ForeignKey(
@@ -34,6 +35,17 @@ class Comment(models.Model):
     def __str__(self):
         return f"Comment by {self.author} on {self.post}"
 
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='liked_posts')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
+        ordering = ['timestamp']
+
+    def __str__(self):
+        return f"{self.user.username} liked {self.post.id}"
 
 
 
