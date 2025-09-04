059569761c88720861ca029c1568c8cbc4a6ffd7
Ndata <wisdomndata@gmail.com>
2025-09-04 18:11:52 +0100

    Social Media API

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -1,26 +1,39 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
+from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
-User = get_user_model()
+# User = get_user_model()
 # Create your models here.
 class Notification(models.Model):
-    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notification')
-    actor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='actor')
+    recipient =  models.ForeignKey(
+    settings.AUTH_USER_MODEL,
+    on_delete=models.CASCADE,
+    related_name="notifications"
+)
+    actor = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="actions"
+)
     verb = models.CharField(max_length=300)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveBigIntegerField()
     target = GenericForeignKey("content_type", "object_id")
 
     preview_text = models.TextField(blank=True, null=True)
-    terget_url = models.URLField(blank=True, null=True)
+    target_url = models.URLField(blank=True, null=True)
 
     is_read = models.BooleanField(default=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
 
-    class Mets:
-        ordering = ["created_at"]
+    class Meta:
+        ordering = ["-created_at"]
     
     def __str__(self):
         return f"Notification to {self.recipient} - {self.verb}"
